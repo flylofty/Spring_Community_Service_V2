154de9ffb89565436857b9b06aecae245d00ea47
Youngnam Lee <lyn00227@naver.com>
2022-05-24 13:39:45 +0900

    modified

--- src/main/java/spartayn/shop/community/domain/BoardRepository.java
+++ src/main/java/spartayn/shop/community/domain/BoardRepository.java
@@ -1,12 +1,11 @@
 package spartayn.shop.community.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-    @Query("SELECT b.id, b.title, b.writer, b.createdAt FROM Board b ORDER BY b.createdAt DESC")
-    List<Board> getMainPageBoards();
+    // 작성 시간 내림차순 정보 얻기
+    List<Board> findAllByOrderByModifiedAtDesc();
 }
