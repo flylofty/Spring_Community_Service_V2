bf9a2dddc35368d6c509a4e4186b6a6e411b79ce
Youngnam Lee <lyn00227@naver.com>
2022-05-24 18:48:36 +0900

    add instance variable

--- src/main/java/spartayn/shop/community/domain/BoardMainPageResponseDto.java
+++ src/main/java/spartayn/shop/community/domain/BoardMainPageResponseDto.java
@@ -10,10 +10,12 @@ public class BoardMainPageResponseDto {
     /**
      * 메인 페이지 정보 (제목, 작성자, 날짜) 전달 DTO
      */
+    private String id;
     private String title;
     private String writer;
     private String createdAt;
     public BoardMainPageResponseDto(Board entity) {
+        this.id = String.valueOf(entity.getId());
         this.title = entity.getTitle();
         this.writer = entity.getWriter();
         this.createdAt = entity.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm"));
