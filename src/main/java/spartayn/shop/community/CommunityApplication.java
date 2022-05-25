4043b1e66344174c75b45a2aee674e47a3092ec1
Youngnam Lee <lyn00227@naver.com>
2022-05-25 19:43:53 +0900

    add propertySource annotation

--- src/main/java/spartayn/shop/community/CommunityApplication.java
+++ src/main/java/spartayn/shop/community/CommunityApplication.java
@@ -2,9 +2,11 @@ package spartayn.shop.community;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
+@PropertySource(value = { "classpath:application-db.properties" })
 @SpringBootApplication
 public class CommunityApplication {
 
